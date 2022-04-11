5f2bb5fe9cd6ee878a5911fc6729293c785889c3
Niels Bosman <nielsbosman@mail.com>
2022-04-11 18:24:54 +0200

    Added basic models

---
+++ Models/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace LINQ.Models;
+
+public class Game
+{
+    private string? Name { get; set; }
+    private string? Genre { get; set; }
+    private int ReleaseYear { get; set; }
+    private string? GameStudio { get; set; }
+    private int Downloads { get; set; }
+    private Platform[]? Platforms { get; set; }
+}
---
+++ Models/Platform.cs
@@ -0,0 +1,6 @@
+﻿namespace LINQ.Models;
+
+public class Platform
+{
+    public string? Name { get; set; }
+}
