3405764a2f9dfdff163a8400df939b1cbd6e20d0
Niels Bosman <nielsbosman@mail.com>
2022-04-11 20:52:22 +0200

    Added orderby LINQ example

---
+++ Examples/OrderByLinqExample.cs
@@ -0,0 +1,25 @@
+﻿using LINQ.Models;
+
+namespace LINQ.Examples;
+
+public class OrderByLinqExample : ILinqExample
+{
+    public string? Description => "We orderen de lijst van games op basis van de hoeveelheid sales";
+    
+    public List<Game> WithQuery(List<Game> games)
+    {
+        return games
+            .OrderBy(game => game.Sales)
+            .ToList();
+    }
+
+    public List<Game> WithMethod(List<Game> games)
+    {
+        var result =
+            from game in games
+            orderby game.Sales
+            select game;
+
+        return result.ToList();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,22 +8,25 @@ var games = new GameRepository().GetGames();
 
 // LINQ functie vragen
 Console.WriteLine("Welke LINQ functie wil je uitproberen? Opties: where, orderby, select, selectmany, join, groupby, count, min, max, sum, average, skip, take, first, firstordefault");
-var function = Console.ReadLine();
 
-ILinqExample example = function switch
+ILinqExample example = Console.ReadLine() switch
 {
     "where" => new WhereLinqExample(),
+    "orderby" => new OrderByLinqExample()
 };
+
+Console.Clear();
 
 // LINQ Variant vragen
 Console.WriteLine("Welke variant daarvan wil je proberen? Opties: method, query");
-var variant = Console.ReadLine();
 
-IEnumerable<Game> filteredList = variant switch
+IEnumerable<Game> filteredList = Console.ReadLine() switch
 {
     "method" => example.WithMethod(games),
     "query" => example.WithQuery(games),
 };
+
+Console.Clear();
 
 // Resultaat tonen
 Console.WriteLine(example.Description);
