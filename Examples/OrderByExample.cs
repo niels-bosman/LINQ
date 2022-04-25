2309416b2756c45e7c7bc5f237795ed29f679021
Niels Bosman <nielsbosman@mail.com>
2022-04-25 22:07:34 +0200

    Tested version

--- Examples/OrderByExample.cs
+++ Examples/OrderByExample.cs
@@ -2,13 +2,13 @@
 
 public class OrderByExample : Example
 {
-    protected override string Description => "We sorteren de lijst van games op basis van de hoeveelheid sales, en daarna het releasejaar";
+    protected override string Description => "We sorteren de lijst van games op basis van de hoeveelheid sales, en daarna het releasejaar. Beide aflopend.";
 
     protected override void RunQueryVariant()
     {
         var list =
             from game in Games
-            orderby game.Sales, game.ReleaseYear
+            orderby game.Sales descending, game.ReleaseYear descending
             select game;
         
         Display(list);
@@ -17,8 +17,8 @@ public class OrderByExample : Example
     protected override void RunMethodVariant()
     {
         var list = Games
-            .OrderBy(game => game.Sales)
-            .ThenBy(game => game.ReleaseYear);
+            .OrderByDescending(game => game.Sales)
+            .ThenByDescending(game => game.ReleaseYear);
         
         Display(list);
     }
